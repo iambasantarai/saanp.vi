c3912075d146f3448a0efdac4791e62d269d7ecb
iamsampang <iamsampang@gmail.com>
2023-10-16 20:37:46 +0545

    Create basic SDL application

--- Makefile
+++ Makefile
@@ -1,12 +1,14 @@
 CC = gcc
 CFLAGS = -Wall -Wextra
+LIBS = -lSDL2
+INCLUDES = -I/usr/include/SDL2/
 SRCS = main.c
 OBJS = ${SRCS:.c=.o}
 MAIN = main.out
 
 $(MAIN) : ${OBJS}
-	$(CC) $(CFLAGS) -o $(MAIN) $(OBJS)
+	$(CC) $(CFLAGS) $(INCLUDES) -o $(MAIN) $(OBJS) $(LIBS)
 .c.o:
-	$(CC) $(CFLAGS) -c $< -o $@
+	$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
 clean:
 	$(RM) *.o *~ $(MAIN)
--- main.c
+++ main.c
@@ -1,7 +1,61 @@
 #include <stdio.h>
 
+#include <SDL2/SDL.h>
+
+#define WINDOW_X 100
+#define WINDOW_Y 100
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
 int main()
 {
-    printf("Hello world!\n");
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "Could not initialize sdl2: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("Saanp",
+                                          WINDOW_X,
+                                          WINDOW_Y,
+                                          WINDOW_WIDTH,
+                                          WINDOW_HEIGHT,
+                                          SDL_WINDOW_SHOWN
+                                          );
+
+    if (window == NULL) {
+        fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
+                                                SDL_RENDERER_ACCELERATED |
+                                                SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == NULL) {
+        SDL_DestroyWindow(window);
+        fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Event event;
+    int quit = 0;
+
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    quit = 1;
+                    break;
+                default: {}
+            }
+        }
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
