c57b0e73b1ef9f55e1dca0a7948feaa6efae2b38
iamsampang <iamsampang@gmail.com>
2023-10-22 20:40:14 +0545

    Render and move segment of saanp

--- main.c
+++ main.c
@@ -8,6 +8,71 @@
 #define ROWS 20
 #define COLS 20
 #define CELL_SIZE 20
+#define SPEED 200
+
+typedef enum { UP, DOWN, LEFT, RIGHT } Direction;
+
+typedef struct Saanp {
+    int x;
+    int y;
+    Direction direction;
+    struct Saanp *next;
+} Saanp;
+
+Saanp *head;
+Saanp *tail;
+
+// initialize saanp
+void init_saanp() {
+    Saanp *saanp = malloc(sizeof(Saanp));
+
+    saanp->x = (rand() % COLS) * CELL_SIZE;
+    saanp->y = (rand() % ROWS) * CELL_SIZE;
+    saanp->direction = RIGHT;
+
+    saanp->next = NULL;
+
+    head = saanp;
+    tail = saanp;
+}
+
+// render saanp
+void render_saanp(SDL_Renderer *renderer, int x, int y) {
+    SDL_SetRenderDrawColor(renderer, 0x45, 0xFE, 0x02, 255);
+
+    SDL_Rect segment;
+    segment.w = CELL_SIZE;
+    segment.h = CELL_SIZE;
+
+    Saanp *current = head;
+
+    while (current != NULL) {
+        segment.x = x + current->x;
+        segment.y = y + current->y;
+
+        SDL_RenderFillRect(renderer, &segment);
+
+        current = current->next;
+    }
+}
+
+// move saanp
+void move_saanp() {
+    switch (head->direction) {
+        case UP:
+            head->y -= ROWS;
+            break;
+        case DOWN:
+            head->y += ROWS;
+            break;
+        case LEFT:
+            head->x -= COLS;
+            break;
+        case RIGHT:
+            head->x += COLS;
+            break;
+    }
+}
 
 // render a grid
 void render_grid(SDL_Renderer * renderer, int x, int y) {
@@ -27,6 +92,9 @@ void render_grid(SDL_Renderer * renderer, int x, int y) {
 }
 
 int main() {
+    // initialize saanp
+    init_saanp();
+
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         fprintf(stderr, "Could not initialize sdl2: %s\n", SDL_GetError());
         return EXIT_FAILURE;
@@ -72,11 +140,15 @@ int main() {
         SDL_RenderClear(renderer);
         // RENDER LOOP START
 
+        move_saanp();
         render_grid(renderer, grid_x, grid_y);
+        render_saanp(renderer, grid_x, grid_y);
 
         // RENDER LOOP END
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderPresent(renderer);
+
+        SDL_Delay(SPEED);
     }
 
     SDL_DestroyRenderer(renderer);
