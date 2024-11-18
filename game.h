9cdf8478450f8999360c617a9ec258982024c046
iambasantarai <iambasantarai@gmail.com>
2024-11-18 19:52:43 +0545

    Extract constants to a seperate header file

---
+++ game.h
@@ -0,0 +1,12 @@
+#ifndef GAME_H_
+#define GAME_H_
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
+#define ROWS 20
+#define COLS 20
+#define CELL_SIZE 20
+#define SPEED 200
+
+#endif
--- main.c
+++ main.c
@@ -4,13 +4,7 @@
 
 #include <SDL2/SDL.h>
 
-#define WINDOW_WIDTH 640
-#define WINDOW_HEIGHT 480
-
-#define ROWS 20
-#define COLS 20
-#define CELL_SIZE 20
-#define SPEED 200
+#include "./game.h"
 
 typedef enum { UP, DOWN, LEFT, RIGHT } Direction;
 
